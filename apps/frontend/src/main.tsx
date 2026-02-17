d11530bef7635af266900fc3fb82385d97494cb7
donizer <bogdan.ua45@gmail.com>
2026-02-17 23:24:21 +0200

    feat: add QueryProvider for improved query management and structure

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,25 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter } from "react-router-dom";
-import { queryClient } from "@/lib/query-client";
-import { queryPersistenceOptions } from "@/lib/query-persistence";
+import { QueryProvider } from "./lib/providers.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PersistQueryClientProvider
-      client={queryClient}
-      persistOptions={queryPersistenceOptions}
-    >
+    <QueryProvider>
       <BrowserRouter>
         <TooltipProvider>
           <App />
         </TooltipProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
-    </PersistQueryClientProvider>
+    </QueryProvider>
   </StrictMode>,
 );
